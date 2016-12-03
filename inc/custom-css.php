d666711b04be02c89de0fc5bcee4cd487de3515b
Renkin42 <arleydecker@renkin42.net>
2016-12-02 22:44:46 -0800

    Fixed using transparent images as backgrounds

--- inc/custom-css.php
+++ inc/custom-css.php
@@ -1,7 +1,8 @@
 <style>
     <?php if ( get_theme_mod( 'content_background' ) ): ?>
-        .content-box{
-            background-image: url("<?php echo get_theme_mod( 'content_background' ); ?>");
-        }
+    .content-padding .content-box{
+        background-color: transparent;
+        background-image: url("<?php echo get_theme_mod( 'content_background' ); ?>");
+    }
     <?php endif; ?>
 </style>
