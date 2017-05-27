af22776556c85db30126b640c1a0d3a4282c5619
Renkin42 <arleydecker@renkin42.net>
2017-05-27 00:38:26 -0700

    Add sass files

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 
 function wootravel_enqueue_scripts() {
     wp_enqueue_style( 'Font_Awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
-    wp_enqueue_style( 'Roboto', 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700' );
     wp_enqueue_style( 'Bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' );
     wp_enqueue_style( 'MDB', 'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css' );
     wp_enqueue_style( 'Wootravel', get_template_directory_uri() . '/css/wootravel.min.css' );
