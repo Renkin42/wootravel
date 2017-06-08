84ead06f707e8f176722906fed6256f76c792379
Renkin42 <arleydecker@renkin42.net>
2017-06-08 10:16:44 -0700

    Organized products and added events section

--- compile-sass.bat
+++ compile-sass.bat
@@ -1,7 +1,7 @@
 @echo off
 rem This is a simple batch script to compile sass files to css and min.css files.
 
-call sass scss\wootravel.scss css\wootravel.css --style expanded
+call sass sass\wootravel.scss css\wootravel.css --style expanded
 echo Full CSS Updated!
-call sass scss\wootravel.scss css\wootravel.min.css --style compressed
+call sass sass\wootravel.scss css\wootravel.min.css --style compressed
 echo Minified CSS Updated!
--- index.php
+++ index.php
@@ -6,10 +6,9 @@
         </div>
     </div>
     <div class="row">
-        <div class="col">
-            
+        <div class="col-md-6 pb-1">
             <!--Card-->
-            <div class="card">
+            <div class="card big-card">
 
                 <!--Card image-->
                 <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
@@ -27,53 +26,179 @@
 
             </div>
             <!--/.Card-->
-
         </div>
-        <div class="col">
-            
-            <!--Card-->
-            <div class="card">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col">
+                    <!--Card-->
+                    <div class="card mb-1">
 
-                <!--Card image-->
-                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
-                <!--/.Card image-->
+                        <!--Card image-->
+                        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
 
-                <!--Card content-->
-                <div class="card-block">
-                    <!--Title-->
-                    <h4 class="card-title">Card title</h4>
-                    <!--Text-->
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
                 </div>
-                <!--/.Card content-->
+                <div class="col">
+                    <!--Card-->
+                    <div class="card mb-1">
 
+                        <!--Card image-->
+                        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
             </div>
-            <!--/.Card-->
+            <div class="row">
+                <div class="col">
+                    <!--Card-->
+                    <div class="card mb-1">
 
+                        <!--Card image-->
+                        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
+                <div class="col">
+                    <!--Card-->
+                    <div class="card mb-1">
+
+                        <!--Card image-->
+                        <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+
+                    </div>
+                    <!--/.Card-->
+                </div>
+            </div>
         </div>
+    </div>
+    <div id="upcoming-events" class="row">
+        <div class="col primary-color white-text text-center p-1 mb-1 z-depth-1">
+            <h1>Upcoming Events</h1>
+        </div>
+    </div>
+    <div class="row">
         <div class="col">
-            
             <!--Card-->
-            <div class="card">
-
-                <!--Card image-->
-                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
-                <!--/.Card image-->
-
-                <!--Card content-->
-                <div class="card-block">
-                    <!--Title-->
-                    <h4 class="card-title">Card title</h4>
-                    <!--Text-->
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+            <div class="card mb-1">
+                <div class="row">
+                    <div class="col event-image">
+                        <!--Card image-->
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+                    </div>
+                    <div class="col">
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+                    </div>
                 </div>
-                <!--/.Card content-->
-
             </div>
             <!--/.Card-->
-
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <!--Card-->
+            <div class="card mb-1">
+                <div class="row">
+                    <div class="col event-image">
+                        <!--Card image-->
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+                    </div>
+                    <div class="col">
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+                    </div>
+                </div>
+            </div>
+            <!--/.Card-->
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <!--Card-->
+            <div class="card mb-1">
+                <div class="row">
+                    <div class="col event-image">
+                        <!--Card image-->
+                        <img class="img-responsive" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                        <!--/.Card image-->
+                    </div>
+                    <div class="col">
+                        <!--Card content-->
+                        <div class="card-block">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                        <!--/.Card content-->
+                    </div>
+                </div>
+            </div>
+            <!--/.Card-->
         </div>
     </div>
 </div>
