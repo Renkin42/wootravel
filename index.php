cee627ac905a470274ee03848e3cd1557eff201e
Renkin42 <arleydecker@renkin42.net>
2017-07-11 13:51:28 -0700

    Added News section

--- index.php
+++ index.php
@@ -232,6 +232,76 @@
             <!--/.Card-->
         </div>
     </div>
+    <div id="news" class="row">
+        <div class="col theme-primary-color white-text text-center p-1 mb-1 z-depth-1">
+            <h1 class="mb-0">News</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <!--Card-->
+            <div class="card mb-1">
+
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn theme-primary-color">Button</a>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <div class="col-sm-4">
+            <!--Card-->
+            <div class="card mb-1">
+
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn theme-primary-color">Button</a>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <div class="col-sm-4">
+            <!--Card-->
+            <div class="card mb-1">
+
+                <!--Card image-->
+                <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg" alt="Card image cap">
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block">
+                    <!--Title-->
+                    <h4 class="card-title">Card title</h4>
+                    <!--Text-->
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn theme-primary-color">Button</a>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+    </div>
 </div>
 <?php get_sidebar(); ?>    
 <?php get_footer(); ?>
