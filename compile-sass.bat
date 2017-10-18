1dd3cd4783416b616a5575ad69092f158496c1e0
Renkin42 <arleydecker@renkin42.net>
2017-10-18 12:39:51 -0700

    Replace wootravel.css with correctly minified style.css

--- compile-sass.bat
+++ compile-sass.bat
@@ -1,7 +1,4 @@
 @echo off
 rem This is a simple batch script to compile sass files to css and min.css files.
 
-call sass sass\wootravel.scss css\wootravel.css --style expanded
-echo Full CSS Updated!
-call sass sass\wootravel.scss css\wootravel.min.css --style compressed
-echo Minified CSS Updated!
+sass sass\style.scss style.css --style compressed
--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function wootravel_enqueue_scripts() {
     wp_enqueue_style( 'Font_Awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
     wp_enqueue_style( 'Bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' );
     wp_enqueue_style( 'MDB', 'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css' );
-    wp_enqueue_style( 'Wootravel', get_template_directory_uri() . '/css/wootravel.min.css' );
+    wp_enqueue_style( 'Wootravel', get_stylesheet_uri() );
 
     wp_enqueue_script( 'jQuery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), '3.2.1', true );
     wp_enqueue_script( 'Tether', 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js', array(), '1.4.0', true );
